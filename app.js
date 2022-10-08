c94e2de7a6639384613833dbbfcfbf1da5326ab7
matiasb7 <105875318+matiasb7@users.noreply.github.com>
2022-10-08 16:32:03 -0300

    Update sections, styles. Add Carousel

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
-let test = $(".test")
+let test = $(".test");
 test.on("click", function () {
         test.animate({
-            width: '0'
-        });
+            width: '0'}
+            );
+    test.promise().done(function(){
+        test.animate({
+            width: '190'}
+        );
+
+    })
+
 })
-//
-// let test = document.querySelector(".test");
-//
-// test.addEventListener("click", function () {
-//     alert("da2");
-// })
+
+$('.slides').slick({
+    slidesToShow: 5,
+    dots:true,
+    centerMode: true,
+    centerPadding: 0,
+    initialSlide: 2,
+    focusOnSelect: true,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                slidesToShow: 3
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                slidesToShow: 1
+            }
+        }
+    ]
+});
